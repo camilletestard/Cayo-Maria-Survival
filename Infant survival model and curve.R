4742654072a2fe0fe56f9d1addd015d097c402b1
camilletestard <camille.testard94@gmail.com>
2022-03-03 08:44:10 +0100

    Create infant model script

---
+++ Infant survival model and curve.R
@@ -0,0 +1,30 @@
+###Final Cox PH model, social integration sole covariate###
+setwd('~/Documents/GitHub/Cayo-Maria-Survival/R.Data')
+load('SocialCapital_Infants.RData')
+library(survival)
+library(survminer)
+Scale=365.25
+data = SocialCapital.ALL
+data$sex=toupper(data$sex)
+length(which(data$Survival==1))/nrow(data)
+
+
+#Format data
+data<-within(data,{
+  sex<-factor(sex,labels=c("M","F"))
+  ordrank<-factor(mom.ordrank, labels=c("L","M","H"))
+  group<-factor(group,labels=c("F","V","KK"))  ##Informs the model of the levels within the catagorical covariates (helps identify where the differences are)
+  Age_entry.days<-as.numeric(Age_entry.days)
+  Age_event.days<-as.numeric(Age_event.days)
+  days.in.study<-as.numeric(days.in.study)
+})
+
+#Run model
+fitsocial<-coxme(Surv(Age_entry.days, Age_event.days, Survival)~mom.std.num.partners+ sex +(1|year.prehurr),data=data) #Runs a cox PH model with age as the time scale.
+cz <- cox.zph(fitsocial)
+print(cz)
+ggcoxzph(cz)
+summary(fitsocial)
+
+fitsocial2<-coxme(Surv(Age_entry.days, Age_event.days, Survival)~mom.top.partner +sex +(1|year.prehurr),data=data) #Runs a cox PH model with age as the time scale.
+summary(fitsocial2)
